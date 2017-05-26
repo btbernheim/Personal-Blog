a7ce15d97398ec3c16e0ffd6de4f02222407f75a
Brian Bernheim <btbernheim@gmail.com>
2017-05-25 22:21:26 -0700

    Add controller methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,7 +18,8 @@ class PostsController < ApplicationController
 
 	def show
 		@post = Post.find(params[:id])
-		# @errors = @post.errors.full_messages
+		@errors = @post.errors.full_messages
+		# using flash[:something] is better 
 	end
 
 	def edit
